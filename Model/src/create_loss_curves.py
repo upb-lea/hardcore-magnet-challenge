58e4280b539fa6389efebdde18f8ec2a4be6cfd8
Nikolas Förster <foerster@lea.uni-paderborn.de>
2023-12-19 19:52:08 +0100

    update create loss curves for single graph option

--- Model/src/create_loss_curves.py
+++ Model/src/create_loss_curves.py
@@ -15,8 +15,12 @@ from utils.visualization import visualize_rel_error_report
 
 parser = argparse.ArgumentParser(description="Create loss curves from experiment ID")
 parser.add_argument("experiment_id", help="The experiment uid")
+parser.add_argument("--material", help="material name to plot, e.g. 'A'.")
+parser.add_argument("--seed", help="seed number to plot")
+parser.add_argument("--fold", help="fold number to plot")
+parser.add_argument("--title", nargs='?', const=1, type=int, help="False for no title")
 
-args = parser.parse_args()
+args, optionals = parser.parse_known_args()
 
 # load up meta info
 exp_tab = pd.read_csv(EXP_CSV_PATH, dtype=EXP_CSV_COLS)
@@ -43,57 +47,106 @@ learning_curves = pd.read_csv(f"{PRED_SINK}/learning_curves_cnn_{args.experiment
 
 number_of_folds = meta_info_df.loc[0, 'n_folds']
 
-for material in best_seed_per_material_df["material"]:
+if args.seed is not None and args.fold is not None and args.material is not None:
+    material = args.material
+    seed = int(args.seed)
+    fold = int(args.fold)
+    print(f"User material {material}, seed {seed}, fold {fold}.")
+    print(f"{args.title = }")
 
-    seed_list = np.arange(0, 20)
+    materials_learning_curves = learning_curves.loc[
+        (learning_curves["material"] == material) & (learning_curves["seed"] == seed)]
 
-    for seed in seed_list:
+    number_epochs = len(materials_learning_curves.index)
+    count_epochs = range(0, number_epochs)
+    experiment_tag = best_seed_per_material_df.loc[best_seed_per_material_df["material"] == material]["tag"].to_string(
+        index=False)
 
-        try:
-            # figure out best seed
-            best_seed = best_seed_per_material_df.loc[best_seed_per_material_df["material"] == material]["seed"].to_numpy(dtype=int)[0]
-            materials_learning_curves = learning_curves.loc[(learning_curves["material"] == material) & (learning_curves["seed"] == seed)]
+    fig = plt.figure(
+        figsize=(8.26, 5),
+    )
+    plt.rcParams.update({'font.size': 16})
 
-            # print(f"{materials_learning_curves = }")
+    plt.semilogy(count_epochs, materials_learning_curves[f"loss_trends_train_h_fold_{fold}"], label=f"train_h")
+    plt.semilogy(count_epochs, materials_learning_curves[f"loss_trends_train_p_fold_{fold}"], label=f"train_p")
+    plt.semilogy(count_epochs, materials_learning_curves[f"loss_trends_val_h_fold_{fold}"], label=f"val_h",
+                marker="o", markersize=1)
+    plt.semilogy(count_epochs, materials_learning_curves[f"loss_trends_val_p_fold_{fold}"], label=f"val_p",
+                marker="o", markersize=1)
+    plt.xlabel("Epochs")
+    plt.ylabel("Training or validation loss")
 
-            if materials_learning_curves.empty:
-                break
+    plt.legend()
+    plt.grid(which='both', axis='both', visible=True)
 
-            number_epochs = len(materials_learning_curves.index)
-            count_epochs = range(0, number_epochs)
-            experiment_tag = best_seed_per_material_df.loc[best_seed_per_material_df["material"] == material]["tag"].to_string(index=False)
-
-            fig, axes = plt.subplots(
-                nrows=np.ceil(number_of_folds / 2).astype(int),
-                ncols=2,
-                sharex=True,
-                sharey=True,
-                figsize=(8.26, 8.26),
-            )
-
-            for fold, ax in zip(range(0, number_of_folds + 1), axes.flatten()):
-                ax.semilogy(count_epochs, materials_learning_curves[f"loss_trends_train_h_fold_{fold}"], label=f"train_h")
-                ax.semilogy(count_epochs, materials_learning_curves[f"loss_trends_train_p_fold_{fold}"], label=f"train_p")
-                ax.semilogy(count_epochs, materials_learning_curves[f"loss_trends_val_h_fold_{fold}"], label=f"val_h", marker="o", markersize=1)
-                ax.semilogy(count_epochs, materials_learning_curves[f"loss_trends_val_p_fold_{fold}"], label=f"val_p", marker="o", markersize=1)
-                ax.set_xlabel("epochs")
-                ax.set_ylabel("training or validation loss")
-                ax.set_title(f"Fold {fold}")
-                ax.legend()
-                ax.grid(which='both', axis='both', visible=True)
+    if args.title == 1:
+        plt.title(f"Fold {fold}")
+        plt.suptitle(
+            f"Eperiment {args.experiment_id}, Material {material}, Seed {seed} \n"
+            f"{experiment_tag}",
+            fontweight="bold",
+        )
+    plt.tight_layout()
+    # plt.show()
+    path = PRED_SINK / f"LearnCurve_Exp_single_{args.experiment_id}_Material_{material}_Seed_{seed}_Fold_{fold}.pdf"
+    print(f"Save file to {path}")
+    plt.savefig(path)
 
 
 
-            fig.suptitle(
-                f"Eperiment {args.experiment_id}, Material {material}, Seed {seed} \n"
-                f"{experiment_tag}",
-                fontweight="bold",
-            )
-            plt.tight_layout()
-            # plt.show()
-            if seed == best_seed:
-                plt.savefig(PRED_SINK / f"LearnCurve_Exp_{args.experiment_id}_Material_{material}_Seed_{seed}_best_seed.pdf")
-            else:
-                plt.savefig(PRED_SINK / f"LearnCurve_Exp_{args.experiment_id}_Material_{material}_Seed_{seed}.pdf")
-        except:
-            pass
+else:
+
+    for material in best_seed_per_material_df["material"]:
+
+        seed_list = np.arange(0, 20)
+
+        for seed in seed_list:
+
+            try:
+                # figure out best seed
+                best_seed = best_seed_per_material_df.loc[best_seed_per_material_df["material"] == material]["seed"].to_numpy(dtype=int)[0]
+                materials_learning_curves = learning_curves.loc[(learning_curves["material"] == material) & (learning_curves["seed"] == seed)]
+
+                # print(f"{materials_learning_curves = }")
+
+                if materials_learning_curves.empty:
+                    break
+
+                number_epochs = len(materials_learning_curves.index)
+                count_epochs = range(0, number_epochs)
+                experiment_tag = best_seed_per_material_df.loc[best_seed_per_material_df["material"] == material]["tag"].to_string(index=False)
+
+                fig, axes = plt.subplots(
+                    nrows=np.ceil(number_of_folds / 2).astype(int),
+                    ncols=2,
+                    sharex=True,
+                    sharey=True,
+                    figsize=(8.26, 8.26),
+                )
+
+                for fold, ax in zip(range(0, number_of_folds + 1), axes.flatten()):
+                    ax.semilogy(count_epochs, materials_learning_curves[f"loss_trends_train_h_fold_{fold}"], label=f"train_h")
+                    ax.semilogy(count_epochs, materials_learning_curves[f"loss_trends_train_p_fold_{fold}"], label=f"train_p")
+                    ax.semilogy(count_epochs, materials_learning_curves[f"loss_trends_val_h_fold_{fold}"], label=f"val_h", marker="o", markersize=1)
+                    ax.semilogy(count_epochs, materials_learning_curves[f"loss_trends_val_p_fold_{fold}"], label=f"val_p", marker="o", markersize=1)
+                    ax.set_xlabel("epochs")
+                    ax.set_ylabel("training or validation loss")
+                    ax.set_title(f"Fold {fold}")
+                    ax.legend()
+                    ax.grid(which='both', axis='both', visible=True)
+
+
+
+                fig.suptitle(
+                    f"Eperiment {args.experiment_id}, Material {material}, Seed {seed} \n"
+                    f"{experiment_tag}",
+                    fontweight="bold",
+                )
+                plt.tight_layout()
+                # plt.show()
+                if seed == best_seed:
+                    plt.savefig(PRED_SINK / f"LearnCurve_Exp_{args.experiment_id}_Material_{material}_Seed_{seed}_best_seed.pdf")
+                else:
+                    plt.savefig(PRED_SINK / f"LearnCurve_Exp_{args.experiment_id}_Material_{material}_Seed_{seed}.pdf")
+            except:
+                pass
