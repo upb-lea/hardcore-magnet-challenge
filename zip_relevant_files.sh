576497f6d503e52d65dd960f032426a4ad14d3da
Wilhelm Kirchgässner <kirchgaessner@lea.uni-paderborn.de>
2023-12-20 20:52:18 +0100

    update zip.sh

--- zip_relevant_files.sh
+++ zip_relevant_files.sh
@@ -7,6 +7,7 @@ zip -r "$PWD/PaderbornUniversity_submission_$now.zip" \
     Model/data/*.json \
     Model/data/input/test/Testing/ \
     Model/data/input/test/Training/ \
+    Model/data/output/*meta* \
     Model/data/models/*c9cfe* \
     Model/src/Model_Inference.py \
     Model/src/run_cnn_*.py \
