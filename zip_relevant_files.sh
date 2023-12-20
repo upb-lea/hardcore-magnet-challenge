dbcbc0cb581f3a1cdc55cf122aaa8a7e330cfd0d
Wilhelm Kirchgässner <kirchgaessner@lea.uni-paderborn.de>
2023-12-20 20:35:48 +0100

    add zip bash file

---
+++ zip_relevant_files.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+now=$(date +"%d_%m_%Y")
+printf $now
+zip -r "$PWD/PaderbornUniversity_submission_$now.zip" \
+    Model/data/*.json \
+    Model/data/input/test/Testing/ \
+    Model/data/input/test/Training/ \
+    Model/data/models/*c9cfe* \
+    Model/src/Model_Inference.py \
+    Model/src/run_cnn_*.py \
+    Model/src/utils/*py \
+    Result/ \
+    README.md
+
